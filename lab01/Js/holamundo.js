dd5ca833e45695ab659a858a6d648b625fba820d
vin <vin.villagaray@vallegrande.edu.pe>
2018-11-15 10:05:19 -0500

    Agregue mi primer Js

---
+++ lab01/Js/holamundo.js
@@ -0,0 +1,18 @@
+//Mi Primer mensaje por consola
+console.log('Hola terricola...te escribo desde el más allá...');
+
+//Imprimir otro mensaje de prueba
+console.log('Este es otro mensaje... no te olvides como hacerlo....')
+
+//Imrpimir números
+console.log(3.580);
+
+//Imprimir 5 nombres de distritos
+Console.log('San Vicente, Imperial,Mala, Asia y Bujama')
+
+//Sumar los números de 100 y 500
+console.log(100 + 500);
+
+
+//Comparar los siguientes números 800 y 500
+console.log(800 < 500);
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Mis primeros pasos con JavaScript
 
 Empezaremos a conocer el lenguaje de programación JavaScript
+
+**Descripción de laboratorios**
+- **Laboratorio** En este 
+laboratorio hemos vinculado un archivo Js y ademas hemos impreso texto y numeros en la consola de navegación  
